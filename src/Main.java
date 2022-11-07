ee5b9462194437de5ce26fefc4a4b9a503365448
10489 <1048958325@qq.com>
2022-11-07 09:59:29 +0800

    hello1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0;i <10;i++){
+            System.out.println("Hello world!");
+        }
+        System.out.println("good!");
+
     }
 }
